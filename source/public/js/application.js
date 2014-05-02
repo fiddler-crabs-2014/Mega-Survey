cb3ec70a013447c366f559d36e71086f97a504e8
Justin Phelps <null>
2014-05-02 16:44:15 -0400

    fixed some css, added index

--- source/public/js/application.js
+++ source/public/js/application.js
@@ -1,7 +1,21 @@
-$(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
+$(document).ready(function () {
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
-});
+// send an HTTP DELETE request for the sign-out link
+$('a#sign-out').on("click", function (e) {
+    e.preventDefault();
+    var request = $.ajax({ url: $(this).attr('href'), type: 'delete' });
+    request.done(function () { window.location = "/"; });
+}); //a#sign-out
+
+  $('#sign-in').submit(function (e) {
+    e.preventDefault();
+    $.ajax({
+      type: 'POST',
+      url: '/sessions',
+      data: $("#sign-in").serialize(),
+      success: function(){
+      console.log("User logged in"),
+      window.location = '/';}
+      }); //$ajax
+    }); //#sign-in
+  }); //document ready
