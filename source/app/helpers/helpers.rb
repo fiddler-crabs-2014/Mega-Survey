bb2bb07a62cc994db5540e598cf5e941d9ac33c8
phil london & Shaun McGeever <pair+plondon.shaunmcgeever@devbootcamp.com>
2014-05-03 16:06:19 -0400

    add logic to survey responses

--- source/app/helpers/helpers.rb
+++ source/app/helpers/helpers.rb
@@ -1,3 +1,14 @@
   def average_result(question_id)
     Answer.group("question_id").average(:answer)[question_id]
   end
+
+  def answer_to_text(answer_num, count)
+    singular_responses = ["they really don't like it", "it's just okay", "they're indifferent", "thinks it's good", "they love it!!"]
+    plural_responses = ["they really don't like it", "it's just okay", "they're indifferent", "they think it's good", "they love it!!"]
+    if count ==1
+      return singular_responses[answer_num-1]
+    else
+      return plural_responses[answer_num-1]
+    end
+  end
+
