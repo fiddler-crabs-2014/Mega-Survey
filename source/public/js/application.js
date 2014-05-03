fd134de2fa95785206556221d7ad773985f25063
Timmy Huang <timmyhuang0404@gmail.com>
2014-05-03 11:17:02 -0400

    Add js for add questions dynamically

--- source/public/js/application.js
+++ source/public/js/application.js
@@ -1,11 +1,11 @@
 $(document).ready(function () {
 
-// send an HTTP DELETE request for the sign-out link
-$('#signout').on("click", function (e) {
-    e.preventDefault(console.log("AJAX intercepted logout"));
-    var request = $.ajax({ url: '/signout', type: 'get' });
-    request.done(function () { window.location = "/"; });
-}); //a#sign-out
+  // send an HTTP DELETE request for the sign-out link
+  $('#signout').on("click", function (e) {
+      e.preventDefault(console.log("AJAX intercepted logout"));
+      var request = $.ajax({ url: '/signout', type: 'get' });
+      request.done(function () { window.location = "/"; });
+  }); //a#sign-out
 
   $('#signinform').submit(function(e) {
     e.preventDefault(console.log("ajax intercept"));
@@ -18,4 +18,14 @@ $('#signout').on("click", function (e) {
       window.location.replace("/");}
       }); //$ajax
     }); //#sign-in
+
+  // Dynamic add questions
+  var i = 6;
+  $("#add_question").on("click", function(){
+    var q = "<p id='question"+i+"'>Question "+i+": <input type='text'></p>";
+    $("#add_question").before(q);
+    $("#question"+i).hide().fadeIn(250);
+    i++;
+  })
+
   }); //document ready
