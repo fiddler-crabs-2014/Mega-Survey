2a03e5f277a2873adb407c2f1e434fcdfa0da61a
plondon <plondon@gm.slc.edu>
2014-05-03 20:52:44 -0400

    seed the database and teach js to find more than first num

--- source/db/seeds.rb
+++ source/db/seeds.rb
@@ -1,3 +1,9 @@
+def take_survey(survey)
+  survey.questions.each do |question|
+    question.answers << Answer.create(:answer => rand(1..5))
+  end
+end
+
 phil = User.create(:username => "Phil", :password => "Password")
 shaun = User.create(:username => "Shaun", :password => "test")
 
@@ -12,11 +18,14 @@ Question.create(:question => "How much do you like pizza?"),
 Question.create(:question => "How much do you like cherry pie?"),
 Question.create(:question => "How much do you like milkshakes?")]
 
-questions.each do |question|
-  question.answers << Answer.create(user: shaun, :answer => (1..5).to_a.sample)
+# questions.each do |question|
+#   question.answers << Answer.create(user: shaun, :answer => (1..5).to_a.sample)
+# end
+
+100.times do
+  take_survey(Survey.find(1))
 end
 
 questions.each do |question|
   food_survey.questions << question
 end
-
